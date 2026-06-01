c343a662e0441a726e7590d080efac685b15865b
Abhigyan321 <sdrop6535@gmail.com>
2026-06-01 12:38:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-  const firebaseConfig = {
-    apiKey: "AIzaSyCIO49XM9ULOQpr5_kNuR3_jHw8lD_wkfM",
-  authDomain: "vaultchat-fd7b6.firebaseapp.com",
-  databaseURL: "https://vaultchat-fd7b6-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "vaultchat-fd7b6",
-  storageBucket: "vaultchat-fd7b6.firebasestorage.app",
-  messagingSenderId: "1096891824340",
-  appId: "1:1096891824340:web:e58bbba3091491c887e341"
-  };
-  /* ============================================================ */
+
